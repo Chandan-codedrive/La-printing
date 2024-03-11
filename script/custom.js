67755618981c8d826eb90a1c2782070b7f49b53a
Chandan-codedrive <cc2099338@gmail.com>
2024-03-11 18:58:30 +0530

    technology use option update

--- script/custom.js
+++ script/custom.js
@@ -109,6 +109,29 @@ $(document).ready(function(){
 });
 
 
+$(document).ready(function(){
+    $('.technology-use').slick({
+        slidesToShow: 7,
+        slidesToScroll: -1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
 // light box
 $(document).ready(function() {
     var $lightbox = $('#lightbox');
